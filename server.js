eae91df00c484114da62f5fd316744327639a2de
faizyi <faizi13062000@gmail.com>
2024-11-02 02:19:32 +0500

    2

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const { config } = require("./src/configs/server.config");
-const connectDB = require("./src/db.connect/db.connect");
 const Routes = require("./src/routes/routes");
 const corsOption = require("./src/configs/cors.config");
 const bodyParser = require("body-parser");
-const app = express();
+const { DB_RETRY_LIMIT, DB_RETRY_TIMEOUT } = require("./src/constants/constants");
+
+let connnectionRetries = 0
+async function connectToDB() {
+    try {
+        console.log("Establishing DB connection....")
+        await mongoose.connect(config.dbURI)
+        console.log("DB connected")
+    } catch (error) {
+        if (connnectionRetries < DB_RETRY_LIMIT) {
+            connnectionRetries++
+            // setTimeout(async() => {
+
+            // } , DB_RETRY_TIMEOUT)
+            console.log(`Reconnecting to DB ${connnectionRetries}/${DB_RETRY_LIMIT}`)
+            await new Promise(resolve => setTimeout(resolve, DB_RETRY_TIMEOUT))
+            await connectToDB()
+        } else {
+            process.exit()
+        }
+    }
+}
+
 const PORT = config.appPort;
+const app = express();
+connectToDB()
+    .then(res => console.log("Connected"))
+    .catch(err => console.log("DB NOT Connected"))
 app.use(bodyParser.json({ limit: "50mb" })); // Increase limit for large files
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json())
@@ -17,13 +43,8 @@ app.get("/", (req, res) => {
     res.send("hello")
 })
 
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server started at port no. ${PORT}`);
-    });
-}).catch(error => {
-    console.error("Failed to connect to the database:", error.message);
-});
-
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+})
 
  
